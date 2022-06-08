e8220e7933592a1487dc26e9758188ffe1ad2286
Lawrence Buta <larry.buta@gmail.com>
2022-06-08 18:38:40 -0400

    Oak Course-06-08-2022

---
+++ DAL/UserDAO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DTO;
+
+namespace DAL
+{
+    public class UserDAO: PostContext
+    {
+        public UserDTO GetUserWithUsernameAndPassword(UserDTO model)
+        {
+            UserDTO dto = new UserDTO();
+            T_User user = db.T_User.FirstOrDefault(x => x.Username == model.Username && x.Password == model.Password);
+            if(user != null && user.Id != 0) 
+                dto.Id = user.Id;
+                dto.Username = user.Username;
+                dto.Name = user.NameSurname;
+                dto.Imagepath = user.ImagePath;
+                dto.IsAdmin = user.isAdmin;
+            return dto;
+        }
+    }
+}
--- UI/Areas/Admin/Controllers/LoginController.cs
+++ UI/Areas/Admin/Controllers/LoginController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DTO;
+using BLL;
 
 namespace UI.Areas.Admin.Controllers
 {
     public class LoginController : Controller
     {
+        UserBLL userbll = new UserBLL();
         // GET: Admin/Login
         public ActionResult Index()
         {
-            return View();
+            UserDTO dto = new UserDTO();
+            return View(dto);
+        }
+
+        [HttpPost]
+        public ActionResult Index(UserDTO model)
+        {
+            UserDTO user = userbll.GetUserWithUsernameAndPassword(model);
+            return View(model);
         }
     }
 }
